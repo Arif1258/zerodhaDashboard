91141bbe48e284c31e2e8de29add7532a7efcc37
Sk Arif Ahmed <arifahmedma490@gmail.com>
2025-07-20 23:02:28 +0530

    Update BuyActionWindow.js

--- src/components/BuyActionWindow.js
+++ src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://zerodhabackend-lb15.onrender.com/api/orders", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
